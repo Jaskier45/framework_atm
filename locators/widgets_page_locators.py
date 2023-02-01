e4647331bf13e614ff180a03749df7a85d2a122b
Andriy <zhakun_ak19@nuwm.edu.ua>
2023-02-01 23:48:18 +0200

    Added test for Auto complete page

--- locators/widgets_page_locators.py
+++ locators/widgets_page_locators.py
@@ -10,3 +10,12 @@ class AccordianPageLocators:
 
     THIRD_ITEM_TITLE = (By.CSS_SELECTOR, "div[id='section3Heading']")
     THIRD_ITEM_CONTENT = (By.CSS_SELECTOR, "div[id='section3Content'] p")
+
+
+class AutoCompletePageLocators:
+    MULTIPLE_INPUT = (By.CSS_SELECTOR, "input[id='autoCompleteMultipleInput']")
+    MULTIPLE_VALUE = (By.CSS_SELECTOR, "div[class='css-12jo7m5 auto-complete__multi-value__label']")
+    MULTIPLE_DELETE = (By.CSS_SELECTOR, "div[class='css-1rhbuit-multiValue auto-complete__multi-value'] svg path")
+
+    SINGLE_CONTAINER = (By.CSS_SELECTOR, 'div[class="auto-complete__single-value css-1uccc91-singleValue"]')
+    SINGLE_INPUT = (By.CSS_SELECTOR, "input[id='autoCompleteSingleInput']")
